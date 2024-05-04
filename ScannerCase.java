680b71d2b0bee4c6e98a29086f7d755d871f1397
qiujiawe <1967842929@qq.com>
2024-05-05 01:05:06 +0800

    修复bug

--- ScannerCase.java
+++ ScannerCase.java
@@ -5,5 +5,6 @@ public class ScannerCase {
         System.out.println("请输入您的年龄:");
         int age = sc.nextInt();
         System.out.println(age >= 18 ? "您成年了" : "您还未成年");
+        sc.close();
     }
 }
